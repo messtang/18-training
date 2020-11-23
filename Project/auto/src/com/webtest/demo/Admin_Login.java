bc86a35e3d9108293be65b4dc6513dc2aa02e85a
messtang <2689520115@qq.com>
2020-11-23 16:16:37 +0800

    Update Admin_Login.java

--- Project/auto/src/com/webtest/demo/Admin_Login.java
+++ Project/auto/src/com/webtest/demo/Admin_Login.java
@@ -14,37 +14,36 @@ import com.webtest.core.BaseTest;
 //第二种解决办法,
 public class Admin_Login  extends BaseTest{
 
-//    @Test(dataProviderClass = JDataProvider.class,dataProvider = "loginData")
-//    public void test_Login(String username,String password) {
-//        webtest.open("/index.php/denglu.html");
+    @Test(dataProviderClass = JDataProvider.class,dataProvider = "loginData")
+    public void test_Login(String username,String password) {
+        webtest.open("/index.php/denglu.html");
+
+        webtest.type("name=user", username);
+        webtest.type("name=pwd", password);
+        webtest.click("id=submit");
+        assertTrue(webtest.isTextPresent("剑鱼论坛后台"));
+    }
+
+//	@Test
+//	public void testLogin1() {
 //
-//        webtest.type("name=user", username);
-//        webtest.type("name=pwd", password);
+//		webtest.open("/index.php/denglu.html");
+//
+//		webtest.type("name=user", "admin");
+//		webtest.type("name=pwd", "admin1234");
 //        webtest.click("id=submit");
-//        assertTrue(webtest.isTextPresent("剑鱼论坛后台"));
-//    }
-
-
-	@Test
-	public void testLogin1() {
-
-		webtest.open("/index.php/denglu.html");
-
-		webtest.type("name=user", "admin");
-		webtest.type("name=pwd", "admin1234");
-        webtest.click("id=submit");
-		assertTrue(webtest.isTextPresent("剑鱼论坛后台"));
-	}
-
-	@Test
-	public void testLogin2() {
-
-		webtest.open("/index.php/denglu.html");
-
-		webtest.type("name=user", "admin");
-		webtest.type("name=pwd", "");
-        webtest.click("id=submit");
-		assertTrue(webtest.isTextPresent("剑鱼论坛后台"));
-	}
+//		assertTrue(webtest.isTextPresent("剑鱼论坛后台"));
+//	}
+//
+//	@Test
+//	public void testLogin2() {
+//
+//		webtest.open("/index.php/denglu.html");
+//
+//		webtest.type("name=user", "admin");
+//		webtest.type("name=pwd", "");
+//        webtest.click("id=submit");
+//		assertTrue(webtest.isTextPresent("剑鱼论坛后台"));
+//	}
 
 }
