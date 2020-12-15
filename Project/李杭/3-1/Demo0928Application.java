7e3b8a07b1fa91c3b57a75cff0b2c0c053996f16
messtang <2689520115@qq.com>
2020-12-15 21:54:05 +0800

    1

---
+++ Project/李杭/3-1/Demo0928Application.java
@@ -0,0 +1,15 @@
+package com.example.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@EnableOpenApi
+@SpringBootApplication
+public class Demo0928Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0928Application.class, args);
+    }
+
+}
---
+++ Project/李杭/3-1/HttpDriver.java
@@ -0,0 +1,119 @@
+package com.example.platform.utils;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.cookie.Cookie;
+import org.apache.hc.client5.http.cookie.CookieStore;
+import org.apache.hc.client5.http.cookie.StandardCookieSpec;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+
+import net.sf.json.JSONObject;
+/**
+ * 
+ * @author lihang
+ *
+ */
+public class HttpDriver {
+	
+	public static JSONObject doGet(String url,CookieStore cookieStore) throws ParseException, IOException {
+		RequestConfig config=RequestConfig.custom().setCookieSpec(StandardCookieSpec.STRICT).build();
+		CloseableHttpClient httpClient=HttpClients.custom().setDefaultRequestConfig(config).setDefaultCookieStore(cookieStore).build();
+		HttpGet httpGet=new HttpGet(ReadProperties.getPropertyValue("Host")+url);
+		CloseableHttpResponse httpResponse=httpClient.execute(httpGet);
+		HttpEntity entity=httpResponse.getEntity();
+		String result=EntityUtils.toString(entity,"UTF-8");
+		System.out.println(result);
+		EntityUtils.consume(entity);
+		httpResponse.close();
+		httpClient.close();
+		
+		List<Cookie> listCookies=cookieStore.getCookies();
+		for (Cookie cookie : listCookies) {
+			System.out.println(cookie.getName()+":"+cookie.getValue());
+		}
+		
+		return JSONObject.fromObject(result);//����������
+	}
+
+	public static JSONObject doGet(String url) throws ParseException, IOException {
+		CloseableHttpClient httpClient=HttpClients.createDefault();
+		HttpGet httpGet=new HttpGet(ReadProperties.getPropertyValue("Host")+url);
+		CloseableHttpResponse httpResponse=httpClient.execute(httpGet);
+		HttpEntity entity=httpResponse.getEntity();//��Ӧ��
+//		httpResponse.getCode();//״̬��
+//		httpResponse.getHeaders();//��Ӧͷ
+		String result=EntityUtils.toString(entity,"UTF-8");
+		System.out.println(result);
+		EntityUtils.consume(entity);
+		httpResponse.close();
+		httpClient.close();
+		return JSONObject.fromObject(result);//����������
+		
+	}
+	
+	public static void doGet2(String url) throws ParseException, IOException {
+		CloseableHttpClient httpClient=HttpClients.createDefault();
+		HttpGet httpGet=new HttpGet(ReadProperties.getPropertyValue("Host")+url);
+		CloseableHttpResponse httpResponse=httpClient.execute(httpGet);
+		HttpEntity entity=httpResponse.getEntity();
+		String result=EntityUtils.toString(entity,"UTF-8");
+		System.out.println(httpResponse.getCode());
+		EntityUtils.consume(entity);
+		httpResponse.close();
+		httpClient.close();
+	}
+	
+	public static JSONObject doGet(String url,String para) throws ParseException, IOException {
+		CloseableHttpClient httpClient=HttpClients.createDefault();
+		HttpGet httpGet=new HttpGet(ReadProperties.getPropertyValue("Host")+url+"?"+para);
+		CloseableHttpResponse httpResponse=httpClient.execute(httpGet);
+		HttpEntity entity=httpResponse.getEntity();//��Ӧ��
+		String result=EntityUtils.toString(entity,"UTF-8");
+		System.out.println(result);
+		EntityUtils.consume(entity);
+		httpResponse.close();
+		httpClient.close();
+		return JSONObject.fromObject(result);//����������
+		
+	}
+	
+	public static JSONObject doPost(String url,String formBody) throws ParseException, IOException {
+		CloseableHttpClient httpClient=HttpClients.createDefault();
+		HttpPost httpPost=new HttpPost(ReadProperties.getPropertyValue("Host")+url);
+		httpPost.setHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
+		httpPost.setEntity(new StringEntity(formBody));
+		CloseableHttpResponse httpResponse=httpClient.execute(httpPost);
+		HttpEntity entity=httpResponse.getEntity();
+		String result=EntityUtils.toString(entity,"UTF-8");
+		System.out.println(result);
+		EntityUtils.consume(entity);
+		httpResponse.close();
+		httpClient.close();
+		return JSONObject.fromObject(result);
+	}
+	
+	public static JSONObject doPost(String url,JSONObject body) throws ParseException, IOException {
+		CloseableHttpClient httpClient=HttpClients.createDefault();
+		HttpPost httpPost=new HttpPost(ReadProperties.getPropertyValue("Host")+url);
+		httpPost.setHeader("Content-Type","application/json;charset=UTF-8");
+		httpPost.setEntity(new StringEntity(body.toString()));
+		CloseableHttpResponse httpResponse=httpClient.execute(httpPost);
+		HttpEntity entity=httpResponse.getEntity();
+		String result=EntityUtils.toString(entity,"UTF-8");
+		System.out.println(result);
+		EntityUtils.consume(entity);
+		httpResponse.close();
+		httpClient.close();
+		return JSONObject.fromObject(result);
+	}
+}
