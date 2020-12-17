041ca4700b6271e506fe1a0cc5df83a6c0e6f948
messtang <2689520115@qq.com>
2020-12-17 13:10:18 +0800

    1

---
+++ Project/李杭/3-1自动化测试平台-后端/springboot-demo/src/test/java/com/example/platform/mallController0928ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class mallController0928ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
